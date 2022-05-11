fd958d3a474ebb958322a9335d94692cb5f789a7
juancereceda <juanmcereceda@gmail.com>
2022-05-11 18:46:11 -0300

    remove context provider

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import AppRouter from "./routes/AppRouter";
 import "./GlobalStyles.css";
-import CartContextProvider from "./context/CartContextProvider";
 
 function App() {
   return (
     <>
-      <CartContextProvider>
-        <AppRouter />
-      </CartContextProvider>
+      <AppRouter />
     </>
   );
 }
